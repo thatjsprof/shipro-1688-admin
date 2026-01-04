c41df26f0b5dfaadc8a415c460e21062fdb41c4b
davetech <davidrums65@gmail.com>
2026-01-04 17:07:09 +0100

    add ability to create order items and update orders for an order item

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -10,30 +10,31 @@ const Dashboard = () => {
 
   return (
     <div className="mt-6">
-      <div className="mb-12 mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
+      <h2 className="text-lg font-semibold mb-5">Shipro Statistics</h2>
+      <div className="mb-12 mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
         <MetricPill
-          className="px-0"
+          className="p-6 border rounded-lg"
           title="All Time Orders"
-          content={`₦${formatNum(stats?.allTimeOrders ?? 0)}`}
+          content={`${formatNum(stats?.allTimeOrders ?? 0)}`}
           icon={<ShoppingBag strokeWidth={1.2} className="size-8" />}
           isLoading={isLoading}
         />
         <MetricPill
-          className="px-0"
+          className="p-6 border rounded-lg"
           title="Orders this month"
-          content={`₦${formatNum(stats?.thisMonth ?? 0)}`}
+          content={`${formatNum(stats?.thisMonth ?? 0)}`}
           icon={<ShoppingBasket strokeWidth={1.2} className="size-8" />}
           isLoading={isLoading}
         />
         <MetricPill
-          className="px-0"
+          className="p-6 border rounded-lg"
           title="Pending Orders"
-          content={`₦${formatNum(stats?.pendingOrders ?? 0)}`}
+          content={`${formatNum(stats?.pendingOrders ?? 0)}`}
           icon={<Clock strokeWidth={1.2} className="size-8" />}
           isLoading={isLoading}
         />
         <MetricPill
-          className="px-0"
+          className="p-6 border rounded-lg"
           title="All Time Payments"
           content={`₦${formatNum(stats?.allTimeTotalPayments ?? 0)}`}
           icon={<Wallet strokeWidth={1.2} className="size-8" />}
